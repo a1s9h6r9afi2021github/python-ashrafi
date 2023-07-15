d4ddcdf78829818320ef77f754e7adc2818656bb
Ghulam rasol ashrafi <ashrafi.rasoul@gmail.com>
2023-07-15 19:05:49 +0500

    this files are newly added

---
+++ If.py
@@ -0,0 +1,10 @@
+carmodel = 2056
+university = "UCA"
+
+print(carmodel +6)
+print(carmodel *34)
+print(carmodel - 53)
+print(carmodel /11)
+
+print(university)
+
---
+++ app6 .py
@@ -0,0 +1,3 @@
+if 5>2:
+    print("this is true")
+    print("this is second")
