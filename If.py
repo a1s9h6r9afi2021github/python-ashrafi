648020f3d80e2811beaec8ac9c2b0f833a69c1a2
Ghulam rasol ashrafi <ashrafi.rasoul@gmail.com>
2023-07-15 19:30:25 +0500

    new file

--- If.py
+++ If.py
@@ -5,6 +5,12 @@ print(carmodel +6)
 print(carmodel *34)
 print(carmodel - 53)
 print(carmodel /11)
-
+age = 53
 print(university)
+# You 53 in seonds
+print(53*365*24*60*60)
+print(53*365*24*60)
+print(53*365*24)
+# You age in minutes
+# You age in hours
 
---
+++ app7.py
@@ -0,0 +1,5 @@
+# You 53 in seonds
+print(53*365*24*60*60)
+print(53*365*24*60)
+print(53*365*24)
+# You age in minutes
